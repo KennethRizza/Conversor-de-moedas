7685d2af06698695fb04efd50e0f3e7a1951eb65
KennethRizza <kennethrizza0905@gmail.com>
2022-09-26 16:36:11 -0300

    Adicionando mais um moeda ao projeto

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ const select = document.getElementById('currency-select')
 
 const dolar = 5.3
 const euro = 5.9
+const bitcoin = 103235.00
 
 const convertValues = () => {
     const input = document.getElementById('input-real').value
     const realValueText = document.getElementById('real-value-text')
     const currencyValueText = document.getElementById('currency-value-text')
 
-    currencyValueText.innerHTML = input / dolar.toFixed(0)
+    currencyValueText.innerHTML = input / dolar
     realValueText.innerHTML = new Intl.NumberFormat(`pt-br`,{
         currency: `BRL`,
         style: 'currency',
@@ -29,11 +30,20 @@ const convertValues = () => {
             currency: `EUR`,
             style: 'currency',
         }).format(input / euro);
-    
     }
+
+    if (select.value === "Bitcoin") {
+        currencyValueText.innerHTML = new Intl.NumberFormat('en-US',
+            { style: 'currency', 
+            currency: 'BTC' ,
+            maximumSignificantDigits: 2           
+        }).format(input / bitcoin)
+    }               
 }
 
-changeCurrency = () => {
+    
+
+const  changeCurrency = () => {
     const currencyName = document.getElementById('currency-name')
     const currencyImgEua = document.getElementById('currency-img-eua')
 
@@ -46,6 +56,10 @@ changeCurrency = () => {
     if (select.value === "€ Euro") {
         currencyName.innerHTML = "Euro"
         currencyImgEua.src = "./img/europa-img.png"
+    }
+    if (select.value === "Bitcoin") {
+        currencyName.innerHTML = "Bitcoin"
+        currencyImgEua.src = "./img/bitcoin.png"
     }
    
 }
